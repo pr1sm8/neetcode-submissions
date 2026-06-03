03e0935ebead1e624b28be86e57068e3c516ac05
Nikith Sai <53569379+pr1sm8@users.noreply.github.com>
2026-06-03 13:58:28 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.go
@@ -0,0 +1,15 @@
+func isPalindrome(s string) bool {
+    s = strings.ToLower(s)
+    reg := regexp.MustCompile(`[^a-z0-9]+`)
+	s = reg.ReplaceAllString(s, "")
+    fmt.Println(s)
+    runes := []rune(s)
+    j := len(runes) - 1
+    for i := 0; i < min(len(runes)/2 + 1, len(runes)); i ++ {
+        if runes[i] != runes[j] {
+            return false
+        }
+        j--
+    }
+    return true
+}
