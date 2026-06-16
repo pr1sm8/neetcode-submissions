5dc925264f02d723a3765a2650cef0ebb23cb323
Nikith Sai <53569379+pr1sm8@users.noreply.github.com>
2026-06-16 21:04:13 +0530

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.go
@@ -0,0 +1,40 @@
+func minWindow(s string, t string) string {
+	sr := []rune(s)
+	tr := []rune(t)
+	trh := make(map[rune]int)
+	for _, v := range tr {
+		trh[v]++
+	}
+
+	windowh := make(map[rune]int)
+	result := ""
+
+	p0 := 0
+	p1 := -1
+	for {
+		// fmt.Println(p0, p1, isvalid(trh, windowh))
+		if isvalid(trh, windowh) {
+			if p1-p0+1 < len(result) || result == "" {
+				result = string(sr[p0:p1+1])
+			}
+			windowh[ sr[p0] ]--
+			p0++
+		} else {
+			if p1 == len(sr)-1 {
+				break
+			}
+			p1++
+			windowh[ sr[p1] ]++
+		}
+	}
+	return result
+}
+
+func isvalid(trh, windowh map[rune]int) bool {
+	for k, v := range trh {
+		if windowh[k] < v {
+			return false
+		}
+	}
+	return true
+}
